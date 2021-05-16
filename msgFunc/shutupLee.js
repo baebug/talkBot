fa0a110a983672ac121fea18ae876842ce9b3d08
baebug <ufr134@gmail.com>
2021-05-16 16:42:42 +0900

    update Func_Lee

--- msgFunc/shutupLee.js
+++ msgFunc/shutupLee.js
@@ -8,8 +8,29 @@ const scriptName = "template";
  * (string) imageDB.getProfileBase64()
  * (string) packageName
  */
+var isLee = false;
 function response(room, msg, sender, isGroupChat, replier, imageDB, packageName) {
+  var asyncAfterMin = function (callbackFunction) {
+    setTimeout(function () {
+      callbackFunction();
+    }, 60000);
+  };
 
+  var changeFalse = function () {
+    isLee = false;
+  };
+
+  if (sender == "이성영" && isLee == false) {
+    if (msg.includes("죽어")) {
+      isLee = true;
+      replier.reply("어허! 형한테 그런 말 하는거 아니야");
+      asyncAfterMin(changeFalse);
+    } else if (msg.includes("나가")) {
+      isLee = true;
+      replier.reply("강봉권 나가");
+      asyncAfterMin(changeFalse);
+    }
+  }
 }
 
 //아래 4개의 메소드는 액티비티 화면을 수정할때 사용됩니다.
